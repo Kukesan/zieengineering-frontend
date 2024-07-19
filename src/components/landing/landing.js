8458c3e85f912947767a987f7a5d5baf6a2c5602
Kukesan <kkukesan@gmail.com>
2024-07-19 23:56:49 +0530

    contact navigate added

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./landing.css";
 
-const Landing = () => {
+const Landing = ({scrollToSection}) => {
     return (
-      <div className="container">
-        <div className="image-section">
+        <div className="container">
+            <div className="image-section">
+            </div>
+            <div className="text-section">
+                <div className="text-box">
+                    <h2 className="landing-sub-title">C N C</h2>
+                    <h1 className="landing-title">Precision CNC Machining Services: Quality and Innovation at Every Turn</h1>
+                    <p className="landing-description">
+                        At Zie Engineering, we excel in delivering high-quality, precise, and innovative CNC machined parts that meet the diverse needs of our clients.
+                    </p>
+                    <button className="quote-button"  onClick={() => scrollToSection("contact")}>GET A QUOTE</button>
+                </div>
+            </div>
         </div>
-        <div className="text-section">
-        <div className="text-box">
-          <h2 className="landing-sub-title">C N C</h2>
-          <h1 className="landing-title">Precision CNC Machining Services: Quality and Innovation at Every Turn</h1>
-          <p className="landing-description">
-            At Zie Engineering, we excel in delivering high-quality, precise, and innovative CNC machined parts that meet the diverse needs of our clients.
-          </p>
-          <button className="quote-button">GET A QUOTE</button>
-          </div>
-        </div>
-      </div>
     );
 };
 
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Landing from "../../components/landing/landing";
 import Contact from "../../components/contact/contact";
 import Footer from "../../shared/footer/footer";
 import Header from "../../shared/header/Header";
 
 const Home = () => {
+    const contactRef = useRef(null);
+
+    const scrollToSection = (section) => {
+        const scrollOptions = {
+            behavior: 'smooth',
+            block: 'start'
+        };
+        if (section === 'contact') {
+            contactRef.current.scrollIntoView(scrollOptions);
+        }
+    }
     return (
         <>
             <Header />
-            <Landing />
-            <Contact />
+            <Landing scrollToSection={scrollToSection}/>
+            <div ref={contactRef}>
+                <Contact />
+            </div>
             <Footer />
         </>
     );
